666297ed098f55d63ffbbb2978585c7620154b66
nisdas <nishdas93@gmail.com>
2019-09-29 16:32:19 +0530

    add comment

--- vendor/github.com/karalabe/usb/stub_usb_enabled.go
+++ vendor/github.com/karalabe/usb/stub_usb_enabled.go
@@ -1,6 +1,8 @@
 package usb
 
-
+// A lot of the cgo code in this package has been commented out in bazel, this file is
+// used as a stub in the timebeing so that any other packages using this library can
+// still function properly and will not be broken.
 
 // Enumerate.This doesn't using the real-one so as to avoid building it with bazel
 // as the real file/function introduce cgo deps.
